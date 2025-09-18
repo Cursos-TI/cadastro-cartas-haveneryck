72c7c06d4e99a10c078a9f8fe521d6df4aa76197
haveneryck <haveneryck@gmail.com>
2025-09-18 16:32:26 +0000

    Nível novato: definição da estrutura de dados e constantes iniciais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+#define TOTAL_CARTAS 32
+
+typedef struct {
+    char codigo[4]; // Ex: A01
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+} Carta;
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-
-  // Área para entrada de dados
-
-  // Área para exibição dos dados da cidade
-
-return 0;
-} 
+    return 0;
+}
