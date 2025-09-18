df546fd4a58a52e11bd073dc61f1836d0718b0eb
haveneryck <haveneryck@gmail.com>
2025-09-18 16:45:30 +0000

    Nível novato: Implementação do cadastro interativo das cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,5 +15,23 @@ int main() {
 
     printf("Cadastro das cartas do Super Trunfo - Países\n");
 
+    for (int i = 0; i < TOTAL_CARTAS; i++) {
+        printf("\nCarta %d\n", i + 1);
+        printf("Código (ex: A01): ");
+        scanf("%s", cartas[i].codigo);
+
+        printf("População: ");
+        scanf("%d", &cartas[i].populacao);
+
+        printf("Área (km²): ");
+        scanf("%f", &cartas[i].area);
+
+        printf("PIB (em bilhões): ");
+        scanf("%f", &cartas[i].pib);
+
+        printf("Número de pontos turísticos: ");
+        scanf("%d", &cartas[i].pontosTuristicos);
+    }
+
     return 0;
 }
