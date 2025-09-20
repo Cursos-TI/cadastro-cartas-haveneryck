d3a733e0292f6b602d35c62700460b9e38e3cca9
haveneryck <haveneryck@gmail.com>
2025-09-20 18:13:22 +0000

    Nível aventureiro esafio lógica Super Trunfo: menu interativo com switch para escolha de atributo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -72,7 +72,89 @@ int main() {
     printf("Índice da segunda carta: ");
     scanf("%d", &indice2);
 
-    // A lógica de comparação será implementada no próximo commit
+    // Menu interativo para escolha do atributo
+    int opcao;
+    printf("\nEscolha o atributo para comparação:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos turísticos\n");
+    printf("5 - Densidade populacional\n");
+    printf("Opção: ");
+    scanf("%d", &opcao);
+
+    // Lógica de comparação com switch e if-else aninhado
+    printf("\nComparação entre %s e %s:\n", cartas[indice1].codigo, cartas[indice2].codigo);
+
+    switch (opcao) {
+        case 1:
+            printf("Atributo: População\n");
+            printf("%s: %d habitantes\n", cartas[indice1].codigo, cartas[indice1].populacao);
+            printf("%s: %d habitantes\n", cartas[indice2].codigo, cartas[indice2].populacao);
+            if (cartas[indice1].populacao > cartas[indice2].populacao) {
+                printf("Vencedora: %s\n", cartas[indice1].codigo);
+            } else if (cartas[indice2].populacao > cartas[indice1].populacao) {
+                printf("Vencedora: %s\n", cartas[indice2].codigo);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 2:
+            printf("Atributo: Área\n");
+            printf("%s: %.2f km²\n", cartas[indice1].codigo, cartas[indice1].area);
+            printf("%s: %.2f km²\n", cartas[indice2].codigo, cartas[indice2].area);
+            if (cartas[indice1].area > cartas[indice2].area) {
+                printf("Vencedora: %s\n", cartas[indice1].codigo);
+            } else if (cartas[indice2].area > cartas[indice1].area) {
+                printf("Vencedora: %s\n", cartas[indice2].codigo);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 3:
+            printf("Atributo: PIB\n");
+            printf("%s: %.2f bilhões\n", cartas[indice1].codigo, cartas[indice1].pib);
+            printf("%s: %.2f bilhões\n", cartas[indice2].codigo, cartas[indice2].pib);
+            if (cartas[indice1].pib > cartas[indice2].pib) {
+                printf("Vencedora: %s\n", cartas[indice1].codigo);
+            } else if (cartas[indice2].pib > cartas[indice1].pib) {
+                printf("Vencedora: %s\n", cartas[indice2].codigo);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 4:
+            printf("Atributo: Pontos turísticos\n");
+            printf("%s: %d pontos\n", cartas[indice1].codigo, cartas[indice1].pontosTuristicos);
+            printf("%s: %d pontos\n", cartas[indice2].codigo, cartas[indice2].pontosTuristicos);
+            if (cartas[indice1].pontosTuristicos > cartas[indice2].pontosTuristicos) {
+                printf("Vencedora: %s\n", cartas[indice1].codigo);
+            } else if (cartas[indice2].pontosTuristicos > cartas[indice1].pontosTuristicos) {
+                printf("Vencedora: %s\n", cartas[indice2].codigo);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 5:
+            printf("Atributo: Densidade populacional\n");
+            printf("%s: %.2f hab/km²\n", cartas[indice1].codigo, cartas[indice1].densidadePopulacional);
+            printf("%s: %.2f hab/km²\n", cartas[indice2].codigo, cartas[indice2].densidadePopulacional);
+            if (cartas[indice1].densidadePopulacional < cartas[indice2].densidadePopulacional) {
+                printf("Vencedora: %s\n", cartas[indice1].codigo);
+            } else if (cartas[indice2].densidadePopulacional < cartas[indice1].densidadePopulacional) {
+                printf("Vencedora: %s\n", cartas[indice2].codigo);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        default:
+            printf("Opção inválida! Nenhuma comparação realizada.\n");
+            break;
+    }
 
     return 0;
-}
